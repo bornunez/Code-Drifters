74d0a2e131c78268c582c2e92eaa5ff36a4ac74d
Miguel <mgremo@ucm.es>
2018-03-06 09:37:01 +0100

    Antes del merge

---
+++ HolaSDL/Bullet.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "GameObject.h"
+
+class Bullet : public GameObject
+{
+private:
+
+public:
+	Bullet(Game* game) : GameObject(game) {};
+};
---
+++ HolaSDL/MouseIcon.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+
+MouseIcon::MouseIcon(Game* game, string iconFileName){
+	this->game = game;
+	this->texture = new Texture(game->getRenderer(), iconFileName);
+}
+
+
+void MouseIcon::drawIcon(SDL_Event & e)
+{
+	if (e.type == SDL_MOUSEMOTION) {
+		this->mouseX = e.motion.x;
+		this->mouseY = e.motion.y;
+	}
+	texture->render(mouseX, mouseY);
+}
