06c416a6e9d205cdd657dee77862b0d9d34fb65b
Aramis <gasardan@ucm.es>
2018-04-17 18:19:48 +0200

    Bugs de la animación del prota

--- HolaSDL/CollisionsManager.cpp
+++ HolaSDL/CollisionsManager.cpp
@@ -68,6 +68,7 @@ void CollisionsManager::bulletCollisions()
 	//Colisionamos todas las balas que esten activas
 	for (Bullet* b : bullets) {
 		if (b->isActive()) {
+			b->updateBody();
 			Transform* t = b->getTransform();
 
 			bool hit = false;
