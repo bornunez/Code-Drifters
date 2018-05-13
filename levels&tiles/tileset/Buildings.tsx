4867ffcb366dc6d9459306c3119b72f17481fba0
Aramissss <gasardan@ucm.es>
2018-05-13 19:42:33 +0200

    Salas 1 puerta city :shit:

--- levels&tiles/tileset/Buildings.tsx
+++ levels&tiles/tileset/Buildings.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Buildings" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="512" columns="16">
- <image source="Buildings.png" width="273" height="546"/>
+<tileset name="Buildings" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="976" columns="16">
+ <image source="Buildings.png" width="273" height="1040"/>
 </tileset>
