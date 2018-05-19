5ec5f70016be6096f588ee81593e5b1071be6c59
Unknown <dimart10@ucm.es>
2018-05-19 03:24:32 +0200

    Boss 3 más animaciones jaja si

--- Animations/Bosses/Boss3/Boss3.tsx
+++ Animations/Bosses/Boss3/Boss3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="boss3" tilewidth="64" tileheight="64" tilecount="60" columns="4" margin="0" spacing="0">
- <image source="boss3.png" width="256" height="960"/>
+<tileset name="boss3" tilewidth="64" tileheight="64" tilecount="76" columns="4" margin="0" spacing="0">
+ <image source="boss3.png" width="256" height="1216"/>
 </tileset>
