6f7c3ba741969c6436e4be76ae3886c7ef2e443f
bornunez <bornunez@ucm.es>
2018-05-20 19:43:02 +0200

    Final sin pointer

--- HolaSDL/Final.cpp
+++ HolaSDL/Final.cpp
@@ -5,7 +5,6 @@
 Final::Final()
 {
 	tex = new Texture(game->getRenderer(), "..\\images\\ImFin.png", 1, 20);
-	tiempo = new Timer();
 	destRect = {70*game->getScale(), 30*game->getScale(), 200*game->getScale(), 200* game->getScale() };
 	ResourceManager::getInstance()->getMusic(SadEnd)->play();
 }
@@ -13,20 +12,20 @@ Final::Final()
 
 void Final::update()
 {
-	tiempo->update();
+	tiempo.update();
 	sourceRect = { tex->getFrameWidth()*frame, 0, tex->getFrameWidth(), tex->getFrameHeight()};
-	if (tiempo->TimeSinceTimerCreation > 2 && tiempo->TimeSinceTimerCreation < 3 && auxDegradado > 0)
+	if (tiempo.TimeSinceTimerCreation > 2 && tiempo.TimeSinceTimerCreation < 3 && auxDegradado > 0)
 	{
 		auxDegradado -= Time::getInstance()->DeltaTime;
 		tex->setColor(auxDegradado*125, auxDegradado*125, auxDegradado*125);
 	}
-	else if (tiempo->TimeSinceTimerCreation > 3 && tiempo->TimeSinceTimerCreation < 4 && auxDegradado < degradado)
+	else if (tiempo.TimeSinceTimerCreation > 3 && tiempo.TimeSinceTimerCreation < 4 && auxDegradado < degradado)
 	{
 		auxDegradado += Time::getInstance()->DeltaTime;
 		frame = 1;
 		tex->setColor(auxDegradado * 125, auxDegradado * 125, auxDegradado * 125);
 	}
-	else if (tiempo->TimeSinceTimerCreation > 4 && tiempo->TimeSinceTimerCreation < 4.5f && auxDegradado > 0)
+	else if (tiempo.TimeSinceTimerCreation > 4 && tiempo.TimeSinceTimerCreation < 4.5f && auxDegradado > 0)
 	{
 		frame = 1;
 		auxDegradado -= Time::getInstance()->DeltaTime;
@@ -38,7 +37,7 @@ void Final::update()
 			sonado = true;
 		}
 	}
-	else if (tiempo->TimeSinceTimerCreation > 4.5 && tiempo->TimeSinceTimerCreation < 6 && auxDegradado < degradado)
+	else if (tiempo.TimeSinceTimerCreation > 4.5 && tiempo.TimeSinceTimerCreation < 6 && auxDegradado < degradado)
 	{
 		frame = 2;
 		auxDegradado += Time::getInstance()->DeltaTime;
@@ -51,7 +50,7 @@ void Final::update()
 			tex->setColor(225, 225, 225);
 			luminoso = true;
 		}
-		if (tiempo->TimeSinceTimerCreation > 6 + (i*0.125f) && frame < 19)
+		if (tiempo.TimeSinceTimerCreation > 6 + (i*0.125f) && frame < 19)
 		{
 			frame = 2 + i;
 		}
@@ -78,5 +77,4 @@ void Final::render()
 Final::~Final()
 {
 	delete tex;
-	delete tiempo;
 }
