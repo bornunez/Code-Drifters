d93d261d1eeec8c25fe561fcbfaa52087c0d63a0
alcasa04 <casadotrapote98@gmail.com>
2018-05-21 00:19:48 +0200

    Nada

--- HolaSDL/Final.cpp
+++ HolaSDL/Final.cpp
@@ -4,7 +4,7 @@
 
 Final::Final()
 {
-	tex = new Texture(game->getRenderer(), "..\\images\\ImFin.png", 1, 20);
+	tex = new Texture(game->getRenderer(), "..\\images\\ImFin.png", 1, 23);
 	destRect = {70*game->getScale(), 30*game->getScale(), 200*game->getScale(), 200* game->getScale() };
 	ResourceManager::getInstance()->getMusic(SadEnd)->play();
 }
@@ -14,53 +14,19 @@ void Final::update()
 {
 	tiempo.update();
 	sourceRect = { tex->getFrameWidth()*frame, 0, tex->getFrameWidth(), tex->getFrameHeight()};
-	if (tiempo.TimeSinceTimerCreation > 2 && tiempo.TimeSinceTimerCreation < 3 && auxDegradado > 0)
+	for (int i = 0; i < tex->getNumCols(); i++)
 	{
-		auxDegradado -= Time::getInstance()->DeltaTime;
-		tex->setColor(auxDegradado*125, auxDegradado*125, auxDegradado*125);
-	}
-	else if (tiempo.TimeSinceTimerCreation > 3 && tiempo.TimeSinceTimerCreation < 4 && auxDegradado < degradado)
-	{
-		auxDegradado += Time::getInstance()->DeltaTime;
-		frame = 1;
-		tex->setColor(auxDegradado * 125, auxDegradado * 125, auxDegradado * 125);
-	}
-	else if (tiempo.TimeSinceTimerCreation > 4 && tiempo.TimeSinceTimerCreation < 4.5f && auxDegradado > 0)
-	{
-		frame = 1;
-		auxDegradado -= Time::getInstance()->DeltaTime;
-		tex->setColor(auxDegradado * 255, auxDegradado * 255, auxDegradado * 255);
-
-		if (!sonado)
-		{
-			ResourceManager::getInstance()->getSoundEffect(SwordSlash)->play();
-			sonado = true;
-		}
-	}
-	else if (tiempo.TimeSinceTimerCreation > 4.5 && tiempo.TimeSinceTimerCreation < 6 && auxDegradado < degradado)
-	{
-		frame = 2;
-		auxDegradado += Time::getInstance()->DeltaTime;
-		tex->setColor(auxDegradado * 125, auxDegradado * 125, auxDegradado * 125);
-	}
-	for (int i = 0; i < 17; i++)
-	{
-		if (!luminoso)
-		{
-			tex->setColor(225, 225, 225);
-			luminoso = true;
-		}
-		if (tiempo.TimeSinceTimerCreation > 6 + (i*0.125f) && frame < 19)
+		if (tiempo.TimeSinceTimerCreation >= i*0.125f)
 		{
 			frame = 2 + i;
 		}
 	}
-	if (frame >= 17 && auxDegradado > 0)
+	if (frame >= tex->getNumCols()-1 && auxDegradado > 0)
 	{
 		auxDegradado -= Time::getInstance()->DeltaTime;
 		tex->setColor(auxDegradado * 125, auxDegradado * 125, auxDegradado * 125);
 	}
-	else if(frame >= 17)
+	else if(frame >= tex->getNumCols() - 1)
 	{
 		auxDegradado = 0;
 		tex->setColor(auxDegradado * 125, auxDegradado * 125, auxDegradado * 125);
